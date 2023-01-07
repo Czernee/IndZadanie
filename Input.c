8367005a6629e046de37b35bedd5ed7655407147
Czernee <tehnikvalerij@gmail.com>
2023-01-08 00:52:12 +0300

    obre4en

--- Input.c
+++ Input.c
@@ -5,11 +5,16 @@
 #include <stdio.h>
 #include <string.h>
 
+//---------------------------------------------------------------------------------
+// Функция выделяет память под запись inputTT.txt
 Input* create_Input()
 	{
 	return (Input*)calloc(1, sizeof(Input));
 	}
 
+//---------------------------------------------------------------------------------
+// Функция "инициализирует" входной файл inputTT.txt
+// Вх. данные: входной файл, структура
 void init_Input(FILE* file, Input* structure)
 	{
 	structure->length = 1;
@@ -23,6 +28,8 @@ void init_Input(FILE* file, Input* structure)
 		}
 	}
 
+//---------------------------------------------------------------------------------
+// Функция освобождает память Input
 void delete_Input(Input* structure)
 	{
 	for (int i = 0; i < structure->length; i++)
--- Output.c
+++ Output.c
@@ -10,11 +10,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+//---------------------------------------------------------------------
+// ‘ункци€ выдел€ет место дл€ записи строки outputTT.txt
 Output* create_Output()
 	{
 	return (Output*)calloc(1, sizeof(Output));
 	}
 
+//---------------------------------------------------------------------------------
+// ‘ункци€ "инициализирует" выходной файл outputTT.txt
+// ¬х. данные: входной файл, структура
 void init_Output(Catalog* catalog, Reestr* reestr, Input* input, Output* output)
 	{
 	FILE* ftable = search_Table(get_InputRecord_month(input->input[0]), get_InputRecord_year(input->input[0]));
@@ -45,29 +50,44 @@ void init_Output(Catalog* catalog, Reestr* reestr, Input* input, Output* output)
 		}
 	}
 
+//---------------------------------------------------------------------
+// ‘ункци€ присваивает фамилию и инициалы преподавател€ в структуре
+// ¬х. данные: структура, фамили€ и инициалы
 void set_Output_FamilyIO(Output* structure, char* FamilyIO)
 	{
 	structure->FamilyIO = (char*)realloc(structure->FamilyIO, (strlen(FamilyIO) + 1) * sizeof(char));
 	strcpy(structure->FamilyIO, FamilyIO);
 	}
 
+//---------------------------------------------------------------------
+// ‘ункци€ присваивает мес€ц расчетного периода в структуре
+// ¬х. данные: структура, мес€ц расчетного периода
 void set_Output_Month(Output* structure, char* Month)
 	{
 	structure->Month = (char*)realloc(structure->Month, (strlen(Month) + 1) * sizeof(char));
 	strcpy(structure->Month, Month);
 	}
 
+//---------------------------------------------------------------------
+// ‘ункци€ присваивает год расчетного периода в структуре
+// ¬х. данные: структура, год расчетного периода
 void set_Output_Year(Output* structure, char* Year)
 	{
 	structure->Year = (char*)realloc(structure->Year, (strlen(Year) + 1) * sizeof(char));
 	strcpy(structure->Year, Year);
 	}
 
+//---------------------------------------------------------------------
+// ‘ункци€ присваивает рассчитанное итоговое значение в структуре
+// ¬х. данные: структура, рассчитанное итоговое значение
 void set_Output_Result(Output* structure, double Result)
 	{
 	structure->Result = Result;
 	}
 
+//---------------------------------------------------------------------
+// ‘ункци€ присваивает строку детализации в структуре
+// ¬х. данные: структура, строка детализации
 void set_Output_DetailNum_s(Output* structure, char* DetailNum_s)
 	{
 	structure->DetailNum_s = (char*)realloc(structure->DetailNum_s, (strlen(DetailNum_s) + 1) * sizeof(char));
